256bbadd77263343af150d7c69491116d14cfcb5
Seunghwan <hetzer12@icloud.com>
2020-08-07 15:54:59 +0900

    키보드 입력 시간 확인

--- test.py
+++ test.py
@@ -1,16 +1,18 @@
 from pynput import keyboard
+import time
 
+start_time = time.time()
 
 def on_press(key):
     if key == keyboard.Key.esc:
         return False
-    print(str(key)+"키를 누름")
+    print("{}키를 누름 [{}]".format(str(key), time.time() - start_time))
 
 
 def on_release(key):
     if key == keyboard.Key.esc:
         return False
-    print(str(key)+"키를 땜")
+    print("{}키를 땜 [{}]".format(str(key),time.time() - start_time))
 
 
 listener = keyboard.Listener(on_press=on_press, on_release=on_release)
